2986da3b34ebcf8457ed9a492522d666eefedbc3
rayssa.andrade <rayssa.andrade071@outlook.com>
2021-12-23 14:25:00 -0300

    configurando primeira página

---
+++ lib/pages/products_overview_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  const ProductsOverviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
