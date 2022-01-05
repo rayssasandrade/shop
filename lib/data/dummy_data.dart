681144976a17be22924be907ea752557858cf512
rayssa.andrade <rayssa.andrade071@outlook.com>
2022-01-05 09:43:35 -0300

    alterado e adicionado produtos

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -1,36 +1,51 @@
-
 import 'package:shopapp/models/product.dart';
 
 final dummyProducts = [
   Product(
     id: 'p1',
-    title: 'Red Shirt',
-    description: 'A red shirt - it is pretty red!',
-    price: 29.99,
+    title: 'Calça pantalona',
+    description: 'Calça pantalona branca',
+    price: 199.99,
     imageUrl:
-        'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-  ),
-  Product(
-    id: 'p2',
-    title: 'Trousers',
-    description: 'A nice pair of trousers.',
-    price: 59.99,
-    imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5xEcD2Ea4GzYiyFDKBV8rjxuL6U8GvcULtg&usqp=CAU',
   ),
   Product(
     id: 'p3',
-    title: 'Yellow Scarf',
-    description: 'Warm and cozy - exactly what you need for the winter.',
+    title: 'Cachecol',
+    description: 'Cachecol rendado amarelo feminino',
     price: 19.99,
     imageUrl: 'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
   ),
   Product(
-    id: 'p4',
-    title: 'A Pan',
-    description: 'Prepare any meal you want.',
-    price: 49.99,
+    id: 'p5',
+    title: 'Conjunto',
+    description: 'KIT ccom blusa, saia, bolsa e bota',
+    price: 409.99,
     imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+        'https://i.pinimg.com/236x/61/67/31/616731173ecd5ca5446248672e885c80.jpg',
+  ),
+  Product(
+    id: 'p4',
+    title: 'Casaco',
+    description: 'Casaco masculino branco',
+    price: 229.99,
+    imageUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiEy8hP0-lVuxUAefcfl1dn-SI6jmb2K8Vng&usqp=CAU',
+  ),
+  Product(
+    id: 'p2',
+    title: 'Short branco',
+    description: 'Short branco',
+    price: 39.99,
+    imageUrl:
+        'https://www.todamulher.com.br/wp-content/themes/todamulher/resized-images/4361031-shorts-em-alfaiate-1-195x195-6dd7c1b3.jpg',
+  ),
+  Product(
+    id: 'p6',
+    title: 'Wide Leg',
+    description: 'Calça Wide Leg',
+    price: 215.99,
+    imageUrl:
+        'https://blog.calcadonet.com.br/wp-content/uploads/2021/02/estilos-de-roupa-retro.jpg',
   ),
 ];
